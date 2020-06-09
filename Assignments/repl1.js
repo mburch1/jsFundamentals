d148cdf1d004e0e78699cb5db5cf1d2e373f5cac
MaryClare Burch <mcburch2001@gmail.com>
2020-06-09 09:31:55 -0400

    06/09/20

---
+++ Assignments/repl1.js
@@ -0,0 +1,14 @@
+let movies = {
+    nameOfMovie: 'Onward', 
+    runTime: '1hr 42m', 
+    genre: 'Fantasy/Comedy',
+    characterArray: [
+        {characterOne: {name:  'Ian Lightfoot', age: '16', items: ['staff','hoodie']}},
+        {characterTwo: {name: 'Barley Lightfoot', age: '18', items: ['cards','van']}}
+    ]
+    
+}
+
+console.log(movies.nameOfMovie);
+console.log(movies.runTime);
+console.log(movies.characterArray);
---
+++ Assignments/repl2.js
@@ -0,0 +1,16 @@
+
+
+function forLoop() {
+    for (let i = 0; i <= 10; i++) 
+    if (i % 2 ==0) {
+        console.log('This is an even number');
+    } else if ( i % 2 !== 0) {
+        console.log('This is an odd number');
+    } else {
+        console.log('This can not be a number');
+    }
+    
+    
+    }
+    
+    forLoop()
